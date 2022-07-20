80358af1860556a8a0660c3dbf8f5a9144c2d4b4
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-07-20 15:19:53 +0200

    added project model and project routes

---
+++ models/Project.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const projectSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    description: String,
+    tech: String,
+    endAt: Date,
+    // lastUpdate: String,
+    cards: [{ type: Schema.Types.ObjectId, ref: "Card" }]
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+// //Quan tinguem usuaris, descomentar per fer que la combinació titol+usuari sigui única
+// projectSchema.index({ title: 1, user: 1}, { unique: true });
+
+const Project = model("Project", projectSchema);
+
+module.exports = Project;
--- routes/project_routes/project.routes.js
+++ routes/project_routes/project.routes.js
@@ -1,6 +1,71 @@
-const express = require('express');
+const router = require("express").Router();
+const Project = require("../../models/Project.model");
+const Card = require("../../models/Card.model");
+const mongoose = require('mongoose');
 
-const router = express.Router();
+//  POST /api/projects  -  Creates a new project
+router.post("/new-project", (req, res, next) => {
+  const { title, description, tech } = req.body;
 
+  Project.create({ title, description, tech, cards: [] })
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+});
+
+router.get("/", (req, res, next) => {
+  Project.find()
+    .populate("cards")
+    .then((allProjects) => res.json(allProjects))
+    .catch((err) => res.json(err));
+});
+
+router.get("/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+  console.log("🚀 ~ file: project.routes.js ~ line 24 ~ router.get ~ projectId", projectId)
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  // Each Project document has `cards` array holding `_id`s of Card documents
+  // We use .populate() method to get swap the `_id`s for the actual Card documents
+  Project.findById(projectId)
+    .populate("cards")
+    .then((project) => res.status(200).json(project))
+    .catch((error) => res.json(error));
+});
+
+// PUT  /api/projects/:projectId  -  Updates a specific project by id 
+router.put("/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findByIdAndUpdate(projectId, req.body, { new: true })
+    .then((updatedProject) => res.json(updatedProject))
+    .catch((error) => res.json(error));
+});
+
+// DELETE  /api/projects/:projectId  -  Deletes a specific project by id
+router.delete("/delete/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findByIdAndRemove(projectId)
+    .then(() =>
+      res.json({
+        message: `Project with ${projectId} is removed successfully.`,
+      })
+    )
+    .catch((error) => res.json(error));
+});
 
 module.exports = router;
