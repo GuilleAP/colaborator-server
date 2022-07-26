a0317174ab021b6e68ca0608f22d8cd97272393a
Aleix Marsà <98183799+aleixmarsa@users.noreply.github.com>
2022-07-26 16:53:26 +0200

    added role to user model

--- routes/user_routes/auth.routes.js
+++ routes/user_routes/auth.routes.js
@@ -10,11 +10,12 @@ const saltRounds = 10;
 
 // POST /signup  - Creates a new user in the database
 router.post('/signup', (req, res, next) => {
-	const { email, password, name } = req.body;
+	const { email, password, name, role } = req.body;
+    console.log("🚀 ~ file: auth.routes.js ~ line 14 ~ router.post ~ role", role)
 
 	// Check if email or password or name are provided as empty string
-	if (email === '' || password === '' || name === '') {
-		res.status(400).json({ message: 'Provide email, password and name' });
+	if (email === '' || password === '' || name === '' || role === '') {
+		res.status(400).json({ message: 'Provide email, password, name and role' });
 		return;
 	}
 
@@ -50,15 +51,15 @@ router.post('/signup', (req, res, next) => {
 
 			// Create the new user in the database
 			// We return a pending promise, which allows us to chain another `then`
-			return User.create({ email, password: hashedPassword, name });
+			return User.create({ email, password: hashedPassword, name, role });
 		})
 		.then((createdUser) => {
 			// Deconstruct the newly created user object to omit the password
 			// We should never expose passwords publicly
-			const { email, name, _id } = createdUser;
+			const { email, name, role, _id } = createdUser;
 
 			// Create a new object that doesn't expose the password
-			const user = { email, name, _id };
+			const user = { email, name, role, _id };
 
 			// Send a json response containing the user object
 			res.status(201).json({ user: user });
