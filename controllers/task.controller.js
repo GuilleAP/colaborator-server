f2acea31816f22ae261929dc4fb6aeaca6c76df1
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-08-08 13:49:10 +0200

    added task controller

---
+++ controllers/task.controller.js
@@ -0,0 +1,79 @@
+const Task = require("../models/Card.model");
+const mongoose = require("mongoose");
+
+module.exports = {
+  getAllTasks: (req, res) => {
+    Task.find()
+      .then((allTasks) => {
+        res.status(200).json(allTasks);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  postNewTask: (req, res) => {
+    const projectId = req.params.projectId;
+    const { title, description, color, stat, limitDate } = req.body;
+
+    Task.create({
+      title: title,
+      description: description,
+      stat: stat,
+      project: projectId,
+      color: color,
+      limitDate: limitDate,
+    })
+      .then((newTask) => {
+        res.status(200).json(newTask);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteTask: (req, res) => {
+    const taskId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+      res.status(400).json({ message: "Specified id is not valid" });
+      return;
+    }
+    Task.findByIdAndRemove(taskId)
+      .then(() =>
+        res.json({
+          message: `Project with ${projectId} is removed successfully.`,
+        })
+      )
+      .catch((error) => res.json(error));
+  },
+
+  putTaskState: (req, res) => {
+    Task.findByIdAndUpdate(req.params.id, {
+      stat: req.params.state.toUpperCase(),
+    })
+      .then((TaskUpdated) => {
+        res.status(200).json(TaskUpdated);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  getTask: (req, res) => {
+    const taskId = req.params.id;
+    Task.findById(taskId)
+      .then((taskResponse) => {
+        res.status(200).json(taskResponse);
+      })
+      .catch((error) => res.json(error));
+  },
+
+  putTask: (req, res) => {
+    const { title, description, color, limitDate } = req.body;
+
+    Task.findByIdAndUpdate(req.params.id, {
+      title: title,
+      description: description,
+      color: color,
+      limitDate: limitDate,
+    })
+      .then((TaskUpdated) => {
+        res.json(TaskUpdated);
+      })
+      .catch((err) => res.json(err));
+  },
+};
--- routes/card.routes.js
+++ routes/card.routes.js
@@ -1,106 +1,24 @@
 const router = require("express").Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const TaskController = require("../controllers/task.controller");
 
-const Card = require("../models/Card.model");
 
 /**
- * Ruta para recuperar todas las targetas de un poroyecto
+ * Ruta para recuperar todas las targetas de un proyecto
  */
-router.get('/card/get-cards', (req, res) => {
-
-    Card.find()
-    .then((allCards) => {
-        res.status(200).json(allCards)
-    })
-    .catch(err => res.status(400).json(err));
-});
-
+router.get("/card/get-cards", TaskController.getAllTasks);
 
 /**
  * Ruta para crear una nueva targeta dentro de un proyecto
  */
-router.post('/:projectId/card/new-card', (req, res) => {
-    console.log("Tasca creada: ", req.body)
-    const projectId = req.params.projectId;
-    const {title, description, color, stat, limitDate} = req.body;
-    console.log("BODY: ", req.body)
+router.post("/:projectId/card/new-card", TaskController.postNewTask);
 
-    Card.create({
-        title: title, 
-        description: description, 
-        stat: stat,
-        project: projectId,
-        color: color,
-        limitDate: limitDate
-    })
-    .then((newCardResponse) => {
-        res.status(200).json(newCardResponse)
-    })
-    .catch(err =>  res.status(400).json(err));
-});
+router.put("/card/updateCard/:id/:state", TaskController.putTaskState);
 
-router.put('/card/updateCard/:id/:stat', (req, res) => {
+router.delete("/card/delete/:id", TaskController.deleteTask);
 
-    Card.findByIdAndUpdate(req.params.id, {    
-        stat: req.params.stat.toUpperCase()
-    })
-    .then((cardUpdated) => {
+router.get("/card/edit/:id", TaskController.getTask);
 
-        res.status(200).json(cardUpdated);
-    })
-    .catch(err => res.json(err))
-
-});
-
-router.delete('/card/delete/:id', (req, res) => {
-    const taskId  = req.params.id;
-    console.log(taskId)
-
-    if (!mongoose.Types.ObjectId.isValid(taskId)) {
-        res.status(400).json({ message: "Specified id is not valid" });
-        return;
-    }
-
-    Card.findByIdAndRemove(taskId)
-        .then(() =>
-        res.json({
-            message: `Project with ${projectId} is removed successfully.`,
-        })
-        )
-        .catch((error) => res.json(error));
-})
-
-router.get('/card/edit/:id', (req, res) => {
-
-    const taskId = req.params.id;
-
-    Card.findById(taskId)
-        .then((taskResponse) => {
-            console.log(taskResponse)
-            res.status(200).json(taskResponse);
-        })
-        .catch((error) => res.json(error));
-})
-
-router.put('/card/updateCard/:id', (req, res) => {
-
-    const { title, description, color, limitDate } = req.body;
-
-    Card.findByIdAndUpdate(req.params.id, {    
-        title: title,
-        description: description,
-        color: color,
-        limitDate: limitDate
-    })
-    .then((cardUpdated) => {
-
-        res.json(cardUpdated);
-    })
-    .catch(err => res.json(err))
-
-});
-
-
-
+router.put("/card/updateCard/:id", TaskController.putTask);
 
 module.exports = router;
