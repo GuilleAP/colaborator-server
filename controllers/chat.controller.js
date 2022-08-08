b08d354a9f2ca35822069a44a46308e70e69c4b5
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-08-08 14:42:55 +0200

    added chat controller

---
+++ controllers/chat.controller.js
@@ -0,0 +1,47 @@
+const Chat = require("../models/Chat.model");
+const Message = require("../models/Message.model");
+
+module.exports = {
+  getChat: (req, res) => {
+    const { chatId } = req.params;
+    Message.find({ chatId })
+      .populate("sender")
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => res.json(error));
+  },
+
+  postDirectChat: (req, res) => {
+    const { _id } = req.payload; //User 1
+    const { userId } = req.params; //User 2
+    //Check if chat between the users already exist
+    Chat.findOne({ participants: { $all: [_id, userId] } })
+      .then((foundChat) => {
+        if (foundChat) {
+          res.status(200).json(foundChat);
+        } else {
+          Chat.create({ participants: [_id, userId] }).then((newChat) => {
+            res.status(200).json(newChat);
+          });
+        }
+      })
+      .catch((error) => res.json(error));
+  },
+
+  postProjectChat: (req, res) => {
+    const { projectId } = req.params;
+    //Check if chat between the users already exist
+    Chat.findOne({ participants: projectId })
+      .then((foundChat) => {
+        if (foundChat) {
+          res.status(200).json(foundChat);
+        } else {
+          Chat.create({ participants: projectId }).then((newChat) => {
+            res.status(200).json(newChat);
+          });
+        }
+      })
+      .catch((error) => res.json(error));
+  },
+};
--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -2,39 +2,11 @@ const router = require("express").Router();
 const Chat = require("../models/Chat.model");
 const Message = require("../models/Message.model");
 const mongoose = require("mongoose");
+const chatController = require("../controllers/chat.controller");
 
-router.post("/start/direct-chat/:userId", (req, res, next) => {
-  const { _id } = req.payload; //User 1
-  const { userId } = req.params; //User 2
-  //Check if chat between the users already exist
-  Chat.findOne({ participants: { $all: [_id, userId] } })
-    .then((foundChat) => {
-      if (foundChat) {
-        res.status(200).json(foundChat);
-      } else {
-        Chat.create({ participants: [_id, userId] }).then((newChat) => {
-          res.status(200).json(newChat);
-        });
-      }
-    })
-    .catch((error) => res.json(error));
-});
+router.post("/start/direct-chat/:userId", chatController.postDirectChat);
 
-router.post("/start/project-chat/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
-  //Check if chat between the users already exist
-  Chat.findOne({ participants: projectId })
-    .then((foundChat) => {
-      if (foundChat) {
-        res.status(200).json(foundChat);
-      } else {
-        Chat.create({ participants: projectId }).then((newChat) => {
-          res.status(200).json(newChat);
-        });
-      }
-    })
-    .catch((error) => res.json(error));
-});
+router.post("/start/project-chat/:projectId", chatController.postProjectChat);
 
 router.post("/start/:userId", (req, res, next) => {
   const { _id } = req.payload; //User 1
@@ -53,13 +25,6 @@ router.post("/start/:userId", (req, res, next) => {
     .catch((error) => res.json(error));
 });
 
-router.get("/messages/:chatId", (req, res, next) => {
-  const { chatId } = req.params;
-  Message.find({ chatId })
-    .populate("sender")
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((error) => res.json(error));
-});
+router.get("/messages/:chatId", chatController.getChat);
+
 module.exports = router;
