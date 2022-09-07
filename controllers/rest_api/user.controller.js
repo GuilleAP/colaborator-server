b18275a1ad390b920f335b140c7e0635d4fc5786
aleixmarsa <98183799+aleixmarsa@users.noreply.github.com>
2022-09-07 15:57:37 +0200

    feat user update route

--- controllers/rest_api/user.controller.js
+++ controllers/rest_api/user.controller.js
@@ -6,4 +6,15 @@ module.exports = {
       .then((allUsers) => res.status(200).json(allUsers))
       .catch((err) => res.json(err));
   },
+  putUser: (req, res) => {
+    User.findByIdAndUpdate(req.body._id, req.body, { new: true })
+      .then((updatedUser) => res.status(200).json(updatedUser))
+      .catch((err) => {
+        if (err.code === 11000) {
+          res.status(400).json({ message: "Duplicated user name." });
+          return;
+        }
+        res.status(400).json(err);
+      });
+  },
 };
